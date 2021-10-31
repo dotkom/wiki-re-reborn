12dcd1809bcaee37981d86f77d6062a94ea23698
Julian Grande <juliangr@stud.ntnu.no>
2021-10-31 15:35:07 +0100

    feat: added navbar component and refactored folder structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Navbar from '../layout/navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Navbar />
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
