ce3331a786937f02f90c39220eeaf46775bc0e17
Julian Grande <juliangr@stud.ntnu.no>
2021-11-14 16:19:46 +0100

    🩹 - parsed data to component for testing

--- src/portal/PortalView.tsx
+++ src/portal/PortalView.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Article } from '../../types/types';
+import PortalAlphabeticalArticles from './components/PortalAlphabeticalArticles';
 
-interface PortalViewProps {}
+interface PortalViewProps {
+  articles: Article[];
+}
 
-const PortalView: React.FC<PortalViewProps> = ({}) => {
-  return <>Portal</>;
+const PortalView: React.FC<PortalViewProps> = ({ articles }) => {
+  return (
+    <>
+      <PortalAlphabeticalArticles articles={articles} />
+    </>
+  );
 };
 export default PortalView;
