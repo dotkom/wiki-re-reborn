1b22b19ab4a5b38b34b7e2ddb822da8ec891bab0
Julian Grande <juliangr@stud.ntnu.no>
2021-11-21 13:59:19 +0100

    ✨ - added alphabetic article list in portals (#5)

--- src/portal/components/PortalAlphabeticalArticles.tsx
+++ src/portal/components/PortalAlphabeticalArticles.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, Divider, Flex, Grid } from 'theme-ui';
 import { Article } from '../../../types/types';
 
 interface PortalAlphabeticalArticlesProps {
@@ -11,25 +12,33 @@ const PortalAlphabeticalArticles: React.FC<PortalAlphabeticalArticlesProps> = ({
   });
 
   const groupedByFirstLetter: Map<string, Article[]> = sortedAlphabetically.reduce(
-    (map, article) => map.set(article.title[0], [...(map.get(article.title[0]) || []), article]),
+    (map, article) => map.set(article.title[0].toUpperCase(), [...(map.get(article.title[0]) || []), article]),
     new Map()
   );
 
+  const columns = (groupedByFirstLetter.size / 2) | 0;
+
   return (
-    <>
-      {Array.from(groupedByFirstLetter, ([key, value]) => {
-        return (
-          <div key={key}>
-            <h2>{key}</h2>
-            <ul>
-              {value.map((article) => {
-                return <li key={article._id}>{article.title}</li>;
-              })}
-            </ul>
-          </div>
-        );
-      })}
-    </>
+    <Box>
+      <Flex sx={{ justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+        <h2>Alle artikler (A-Å)</h2>
+        <Divider sx={{ color: '#F9B759', borderBottom: '2px solid', width: '80px' }}></Divider>
+      </Flex>
+      <Grid columns={[columns]}>
+        {Array.from(groupedByFirstLetter, ([key, value]) => {
+          return (
+            <div key={key}>
+              <h3>{key}</h3>
+              <ul>
+                {value.map((article) => {
+                  return <li key={article._id}>{article.title}</li>;
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 };
 
