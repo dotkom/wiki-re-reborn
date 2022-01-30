4f5b9f92f4e41271c8155b965594ae8252fbb514
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2022-01-30 18:26:03 +0100

    ✨ - added a Layout component

--- src/pages/[portal]/index.tsx
+++ src/pages/[portal]/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 import { Article, Portal } from '../../../types/types';
 import { fetcher, useArticlesByPortalSlug } from '../../api/hooks';
+import { Layout } from '../../layout/components/Layout';
 import PortalView from '../../portal/PortalView';
 
 interface PortalIndexProps {
@@ -21,18 +22,9 @@ const PortalIndex: React.FC<PortalIndexProps> = ({ portal }) => {
   if (error) return <>Error...</>;
 
   return (
-    <>
-      {/* <h1>{name} portal</h1>
-      {data.result.map((article: Article, key: number) => (
-        <Link href={`/${current}/${article.slug.current}`} key={key} passHref>
-          <div>
-            <a>{article.title}</a>
-            <p>{article.excerpt}</p>
-          </div>
-        </Link>
-      ))} */}
+    <Layout>
       <PortalView portal={portal} articles={data.result} />
-    </>
+    </Layout>
   );
 };
 
